fbbaf6f6562360cd30a2a5ceeea340e9d2e989af
William <victorwccv@gmail.com>
2023-12-20 22:15:49 -0500

    two first projects

---
+++ feedback-ui/script.js
@@ -0,0 +1,32 @@
+const ratingEls = document.querySelectorAll(".rating");
+const btnEl = document.getElementById('btn');
+const containerEl = document.getElementById('container');
+let selectedRating = '';
+
+ratingEls.forEach((ratingEl) => {
+  ratingEl.addEventListener("click", (event) => {
+    removeActive();
+    selectedRating = event.target.innerText || event.target.parentNode.innerText;
+    event.target.classList.add('active')
+    event.target.parentNode.classList.add('active')
+  });
+});
+
+btnEl.addEventListener('click', () => {
+  if (selectedRating !== "") {
+    containerEl.innerHTML = `
+    <strong style='font-size: 40px;'>Thank you!</strong>
+    <br>
+    <br>
+    <stong>Feedback: ${selectedRating}</stong>
+    <p>We'll use your feedback to improve our customer suport 🚀</p>
+    `
+  }
+})
+
+function removeActive() {
+  ratingEls.forEach(ratingEl => {
+    ratingEl.classList.remove('active');
+  });
+}
+
---
+++ joke-generator/script.js
@@ -0,0 +1,39 @@
+const btnEl = document.getElementById('btn');
+const jokeEl = document.getElementById('joke')
+
+const apiKey = 'dt/aozkljjVlLQMJpwKBMA==tnpUADaFNfSoPk77';
+
+const options = {
+  method: 'GET',
+  headers: {
+    'X-Api-Key': apiKey
+  }
+}
+
+const apiURL = 'https://api.api-ninjas.com/v1/dadjokes?limit=1'
+
+async function getJoke() {
+
+  try {
+    jokeEl.innerText = 'Getting Joke...';
+    btnEl.disabled = true;
+    btnEl.innerText = 'Loading...'
+    const response = await fetch(apiURL, options);
+    const data = await response.json();
+    btnEl.disabled = false;
+    btnEl.innerText = 'tell me a joke'
+    console.log(data[0].joke);
+    jokeEl.innerHTML = data[0].joke;
+    
+  } catch (error) {
+    jokeEl.innerHTML = 'An error happened, Try again later';
+    btnEl.disabled = false;
+    btnEl.innerText = 'tell me a joke'
+    console.log(error);
+  }
+
+}
+
+
+btnEl.addEventListener('click', getJoke)
+
